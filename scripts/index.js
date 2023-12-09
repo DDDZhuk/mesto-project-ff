b554dc4467c9810031a02a89267dc3da84312a33
DDDZhuk <zhuckanin@yandex.ru>
2023-12-09 16:03:40 +0400

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
-
+// @todo: Темплейт карточкиplaces__lis
+const cardTemplate =document.querySelector('#card-template').content;
 // @todo: DOM узлы
+const placesList=document.querySelector ('.places__list');
 
 // @todo: Функция создания карточки
+function craetingCard (name,link,deleteCard){
+    const cardElement=cardTemplate.querySelector ('.card').cloneNode(true);
 
+    cardElement.querySelector('.card__image').src=link;
+    cardElement.querySelector('.card__image').alt=name;
+    cardElement.querySelector('.card__title').textContent =name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function removeCard(event) {
+    const deleteButton = event.target.closest('.card');
+    deleteButton.remove();
+};
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(element) {
+    placesList.append(craetingCard(element.name, element.link, removeCard));
+});
